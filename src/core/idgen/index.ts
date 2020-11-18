d466b8381c6d81b7beca055d2aadb23f02653f8b
lenconda <i@lenconda.top>
2020-11-18 21:11:40 +0800

    fix: wrong syntax in uuid generator

--- src/core/idgen/index.ts
+++ src/core/idgen/index.ts
@@ -2,7 +2,7 @@ import { v3 as uuid, v4 as random } from 'uuid';
 
 const generateUuid = (email: string = 'example@example.com') => {
   const namespace = random();
-  return uuid(`${email}${Date.now().toString}${namespace}`, namespace);
+  return uuid(`${email}${Date.now().toString()}${namespace}`, namespace);
 };
 
 export default generateUuid;
